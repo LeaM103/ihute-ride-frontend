3d720274e96c5e5c79f01990e94daad2263d21dd
unknown <l.abatoni@alustudent.com>
2026-07-17 22:10:46 +0200

    Fix profile update API request

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,9 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  // Replace with your computer's IP address
+  // Backend URL
   static const String baseUrl = "http://192.168.1.7:5000/api/auth";
 
+  // UPDATE PROFILE
   static Future<bool> updateProfile({
     required int id,
     required String name,
@@ -13,9 +14,10 @@ class ApiService {
     required String occupation,
   }) async {
     final response = await http.put(
-      Uri.parse("$baseUrl/update-profile/$id"),
+      Uri.parse("$baseUrl/update-profile"),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode({
+        "id": id,
         "name": name,
         "phone": phone,
         "occupation": occupation,
